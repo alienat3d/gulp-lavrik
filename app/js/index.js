647f7e15917334be07b88d9e49dc27115a35aa59
Al Tsaplin <al.tsaplin@gmail.com>
2023-11-06 05:11:16 +0300

    Add (notes & code) Add more notes & tasks Gulpfile.js

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,3 @@
 console.log(divide(4, 2));
 console.log(divide(10, 5));
+console.log(divide(525, 5));
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
+const gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cleanCSS = require('gulp-clean-css'),
+  uglify = require('gulp-uglify'),
+  del = require('del'),
+  browserSync = require('browser-sync').create();
 
 // Подключим весьма спорным методом Normalize, чтобы потестить concat. Т.к. обычно подключаем через препроцессор или с помощью bower. Но сейчас подключим его напрямую. Хоть способ и топорный, но это самый быстрый способ подключить Normalize.css без препроцессоров.
 // Если бы мы работали с препроцессорами, то всё было куда проще, но здесь мы рассмотрим сборку под CSS, то для упорядочивания подключения создадим массив.
@@ -31,16 +33,53 @@ function styles() {
     }))
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream())
 }
 
 function scripts() {
   return gulp.src(jsFiles)
-  .pipe(concat('main.min.js'))
-  .pipe(uglify({
-    toplevel: true
-  }))
-  .pipe(gulp.dest('./dist/js'))
+    .pipe(concat('main.min.js'))
+    .pipe(uglify({
+      toplevel: true
+    }))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream())
+}
+// Следит за изменениями любых файлов CSS и запускает функцию styles при малейшем изменении. Тоже делаем и для JS-скриптов.
+// Если работаем с PHP, то в настройках к browserSync можно прописать proxy (ссылку на localhost в нём прописать). Можно затуннелить это всё прописав "tunnel: true". Таким образом можно будет смотреть на проект по ссылке вообще из любого места и сети.
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    notify: false,
+    // tunnel: true
+  })
+
+  gulp.watch('./*.html', browserSync.reload)
+  gulp.watch('./app/css/**/*.css', styles)
+  gulp.watch('./app/js/**/*.js', scripts)
+}
+// Функция clean() - поможет нам подчищать старые файлы в папке dist при помощи плагина del, во избежание неприятных багов при перезаписывании. Но как отдельную задачу нам его его не нужно регистрировать, вместо этого мы включим его в специальную задачу "build", которая выполнит ряд действий. Которая при помощи parallel() будет запускать задачи в параллельном режиме.
+function clean() {
+  return del(['dist/*'])
 }
 
 gulp.task('styles', styles);
 gulp.task('scripts', scripts);
+gulp.task('watch', watch);
+// Альтернативно этому можно зарегистрировать функцию Gulp d в новом формате так:
+/* exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch; */
+
+gulp.task('build', gulp.series(clean,
+  gulp.parallel(styles, scripts)
+));
+// Для разработки также сделаем. Кстати, когда у нас таски, а не функции, то можно писать и с одинарными кавычками.
+gulp.task('dev', gulp.series('build', 'watch'))
+/* exports.build.series(clean,
+  parallel(styles, scripts)
+); */
+
+// TODO: Добавить sourcemap, imagemin, font-converter, может PUG, в CSS-стилях разные сборки в прод и в дев, препроцессоры. Для JS либо webpack-stream, либо бревсирифай для модульности. Babel.
